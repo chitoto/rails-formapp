71b8f754eb6e5a7a41006c65d849a83a67f3a6ef
Yumiko Kamiyama <tototina.ruru@gmail.com>
2020-10-02 13:38:40 +0900

    fix.controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,7 +6,7 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.create(name: params[:contact][:name], email: params[:contact][:email], content: params[:contact][:content])
     if @contact.save
-      redirect_to new_contact_path, notice: "お問い合わせを作成しました！"
+      redirect_to new_contact_path
     else
       # 入力フォームを再描画します。
       render :new
